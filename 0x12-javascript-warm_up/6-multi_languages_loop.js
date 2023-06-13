9461b2a6dbcff64caccd11ba03102a645e3d77fb
LitBowya <agbagabenjamin819@gmail.com>
2023-06-13 05:24:09 -0700

    added required files

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let multilingual = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < multilingual.length; i++) {
+    console.log(multilingual[i]);
+
+}
